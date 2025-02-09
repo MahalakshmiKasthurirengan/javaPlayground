74ff0d4048b7cfb8c6190479034c6df842b09f57
MahalakshmiKasthurirengan <mahalakshmikasthurirengan22@gmail.com>
2025-02-09 23:11:28 +0530

    leetcode daily streak

---
+++ Leetcode/2364.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long countBadPairs(int[] nums) {
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        long goodCount = 0;
+        long n = nums.length;
+        for(int i = 0; i < n; i++){
+            int key = nums[i] - i;
+            if(map.containsKey(key)){
+                goodCount += map.get(key);
+            }
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        long totalCount = n * (n - 1) / 2;
+        long badCount = totalCount - goodCount;
+        return badCount;
+    }
+
+}
